ae24c6430ba465006250f2fea47be24dd0341be7
SAADELJARIDA <saadeljarida@gmail.com>
2025-07-16 17:34:48 +0100

    nothing added

--- main.c
+++ main.c
@@ -1,64 +1,105 @@
 #include "minishell.h"
 
-/*This function save the env variables in a varibale (act as global var) glb_list()->env */
-t_env	*save_env(char **env)
-{
-	t_env	*env_list;
-	t_env	*env_head;
-	int		i;
-	int		j;
+t_garbage_collector *g_free;
 
-	i = 0;
-	env_head = malloc(sizeof(t_env));
-	env_list = env_head;
-	while (env[i] != 0)	
+// char	*handle_input(char *input)
+// {
+// 	int	i;
+// 	int	len;
+
+// 	len = ft_strlen(input);
+// 	i = 0;
+// 	while (i )
+// 	if (input[i] == '\'' && input[i + 1] == '\'' ||
+// 		input[i] == '\"' && input[i + 1] == '\"')
+// 	{
+// 		while(i < len)
+// 		{
+
+// 		}
+// 	}
+// }
+
+int main(int ac, char **av, char **env) {
+
+    t_tokenizer *tokens;
+    char *input;
+    int exit_status;
+    char **args;
+    char **envp;
+	char *path;
+
+  (void)ac;
+  (void)av;
+
+  glb_list()->env = NULL;
+  init_env_var(&glb_list()->env, env);
+
+  while (1) {
+    input = readline("\033[1;32m➜\033[0m\033[1;36m Minishell $> \033[0m");
+
+    if (!input) {
+      printf("exit\n");
+      exit(0); // ou free et exit proprement si nécessaire
+    }
+    // check_quo_error();
+    tokens = tokenizer(input);
+    expanding(tokens);
+    print_tokenizer(tokens);
+
+	args = tokens_to_args(tokens);
+	if (args && args[0])
 	{
-		j = 0;
-		while (env[i][j] != '=')
-			j++;
-		env_list->name = ft_substr(env[i], 0, j);
-		env_list->value = ft_substr(env[i], j + 1, ft_strlen(env[i]));
-		i++;
-		if (env[i] != NULL)
-			env_list->next = malloc(sizeof(t_env));
+		exit_status = 0;
+		if (execute_builtin(args, &glb_list()->env, &exit_status) == 1)
+			printf("Builtin command executed successfully.\n");
 		else
-			break;
-		env_list = env_list->next;
-	}
-	env_list->next = NULL;
-	return (env_head);
+		{
+			path = get_cmd_path(args[0], glb_list()->env);
+			if (!path)
+			{
+				ft_putstr_fd("minishell: command not found: ", 2);
+				ft_putstr_fd(args[0], 2);
+				ft_putchar_fd('\n', 2);
+				exit_status = 127;
+			}
+			else
+				{
+					envp = envlist_to_array(glb_list()->env);
+					if (!envp)
+					{
+						perror("envlist_to_array");
+						free(path);
+						exit(1);
+					}
+
+					if (fork() == 0)
+					{
+						execve(path, args, envp);
+						perror("execve");
+						exit(1);
+					}
+					else
+					{
+						wait(&exit_status);
+					}
+					free(path);
+					free_strs(envp);
+				}
+			}
+		}
+//	print_env(glb_list()->env);
+	free_args(args);
+	free_tokens(input, tokens);
+  }
 }
+   // t_ast		*ast;
+    // ast = ast_builder(tokens);
+    // printf("________________NO_THE_TREE_________________");
+    // print_node(ast);
+    // printf("this is the right \n");
+    // print_node(ast->left);
+    // printf("this is the left \n");
+    // print_node(ast->left);
+    // free_all(input, tokens);
 
-int	main(int ac, char **av, char **env)
-{
-
-	t_tokenizer *tokens;
-	char		*input;
-
-	(void)ac;
-	(void)av;
-	//signal_handler();
-	glb_list()->env = save_env(env);
-	while (1)
-	{
-		input = readline("\033[1;32m➜\033[0m\033[1;36m Minishell@damn:$ \033[0m");
-		if (input_error(input) == 1)
-			break;
-		tokens = tokenizer(input);
-		if (check_parsing_errors(tokens))
-			return (1);
-		expanding(tokens);
-		print_tokenizer(tokens);
-		free_tokens(input, tokens);
-		break;
-	}
-	free_env(glb_list()->env);
-}
-		// t_ast		*ast;
-		// ast = ast_builder(tokens);
-		// printf("________________NO_THE_TREE_________________");
-		// print_node(ast);
-		// printf("this is the right \n");
-		// print_node(ast->left);
-		// printf("this is the left \n");
-		// print_node(ast->left);
