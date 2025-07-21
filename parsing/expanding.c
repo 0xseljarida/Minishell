9fa149349360b4fa0331f2874eec807aae24fc95
SAADELJARIDA <saadeljarida@gmail.com>
2025-07-21 17:33:41 +0100

    ls

--- parsing/expanding.c
+++ parsing/expanding.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-void	remove_empty_quote(char *str, int start, int end)
+void	remove_quote(char *str, int start, int end)
 {
 	int	i;
 
@@ -20,156 +20,109 @@ void	remove_empty_quote(char *str, int start, int end)
 	str[ft_strlen(str) - 2] = 0;
 }
 
-int	quote_handling(char *str)
+int	quote_handling(t_tokenizer *token)
 {
 	int		i;
 	char	q;
 	int		start;
+	int		j;
 
 	i = 0;
-	while (str[i] != 0)
+	j = 0;
+	while (token->str[i] != 0)
 	{
-		q = is_quote(str[i]);
-		if (q != 0)
+		q = is_quote(token->str[i]);
+		if (q != 0 && i == (token->quotes_index[j] - j/2))
 		{
+			// printf("glb_list : %d\n", (glb_list()->quotes_index[j] - j/2));
+			// printf("glb_list the second : %d\n", (glb_list()->quotes_index[j] - j/2));
 			start = i;
 			i++;
-			while (q != is_quote(str[i]))
+			j++;
+			while (q != is_quote(token->str[i]) || i != (token->quotes_index[j] - j/2))
 				i++;
-			remove_empty_quote(str, start, i);
+			remove_quote(token->str, start, i);
+			j++;
 			i -= 2;
 		}
+
 		i++;
 	}
 	return (0);
 }
 
 /*for other $? $!...*/
-int	valid_expanding(char *str, int *len)
+
+
+/* TO REMOVE */
+// void	add_env_token_list(t_env_list *env_token_list, char *env_value, int i)
+// {
+// 	t_env_list	*temp;
+
+// 	temp = env_token_list;
+// 	while(temp != NULL)
+// 		temp = temp->next;
+// 	temp = malloc(sizeof(t_env_list));
+// 	temp->str = env_value;
+// 	temp->i = i;
+// 	temp->next = NULL;
+// }
+
+
+
+t_tokenizer	**env_var(t_tokenizer **token)
 {
 	int			i;
+	// int			index;
+	// t_env_list	*env_token_list;
 
-	i = 1;
-	if (!ft_isalnum(str[1]))
-		return (0);
-	if (ft_isdigit(str[1]))
-	{
-		*len = 2;
-		return (1);
-	}
-	while (ft_isalnum(str[i]))
-		i++;
-	*len = i;
-	return (1);
-}
-
-/*void	env_var(t_tokenizer *token)
-{
-	int			i;
-	char		*env_value;
-	int			len;
+	// env_token_list = NULL;
+	// index = 0;
 
 	i = 0;
-	while (token->str[i] != 0)
+	while ((*token)->str[i] != 0)
 	{
-		if (token->str[i] == '\'')
+		if ((*token)->str[i] == '\'')
 		{
 			i++;
-			while (token->str[i] != '\'')
+			while ((*token)->str[i] != '\'')
 				i++;
 		}
-		if (token->str[i] == '$' && valid_expanding(token->str + i, &len))
+		if ((*token)->str[i] == '\"')
 		{
-			env_value = check_env(ft_substr(token->str, i + 1, len - 1));
-			token->str = re_alloc(token->str, i, len, env_value);
+			i++;
+			while ((*token)->str[i] != '\"')
+			{
+				i++;
+			}
 		}
+		expand_nq(token, &i);
 		i++;
 	}
-}*/
-
-static void insert_split_tokens(t_tokenizer *token, char **split_values)
-{
-    t_tokenizer *current = token;
-    int j;
-
-    j = 1;
-    while (split_values[j])
-    {
-        t_tokenizer *new_token = malloc(sizeof(t_tokenizer));
-        new_token->str = ft_strdup(split_values[j]);
-        new_token->op = NOT_OP;
-        new_token->next = current->next;
-        current->next = new_token;
-        current = new_token;
-        j++;
-    }
+	if (to_retokenize(token) == 1)
+		tokenize_the_envar(token);
+	save_index(*token);
+	return (token);
 }
 
-static int process_variable_in_str(t_tokenizer *token, int i)
+void	expanding(t_tokenizer **token)
 {
-    int len;
-    char *env_value;
-    char **split_values;
+	t_tokenizer	**temp;
 
-    if (!valid_expanding(token->str + i, &len))
-        return (i);
-
-    env_value = check_env(ft_substr(token->str, i + 1, len - 1));
-    if (env_value && ft_strchr(env_value, ' '))
-    {
-        split_values = ft_split(env_value, ' ');
-        free(token->str);
-        token->str = ft_strdup(split_values[0]);
-        insert_split_tokens(token, split_values);
-        for (int j = 0; split_values[j]; j++)
-            free(split_values[j]);
-        free(split_values);
-    }
-    else
-        token->str = re_alloc(token->str, i, len, env_value);
-    return (i + len);
-}
-
-void env_var(t_tokenizer *token_list)
-{
-    t_tokenizer *token = token_list;
-    int i;
-
-    while (token)
-    {
-        if (token->op == NOT_OP && token->str)
-        {
-            i = 0;
-            while (token->str[i])
-            {
-                if (token->str[i] == '\'')
-                {
-                    i++;
-                    while (token->str[i] && token->str[i] != '\'')
-                        i++;
-                }
-                else if (token->str[i] == '$')
-                    i = process_variable_in_str(token, i);
-                else
-                    i++;
-            }
-        }
-        token = token->next;
-    }
-}
-
-void	expanding(t_tokenizer *token)
-{
-	while (token != NULL)
+	temp = token;
+	while ((*temp) != NULL)
 	{
-		if (token->op == LESS_LESS)
-			token->hd = here_doc(token);
+		if ((*temp)->op == LESS_LESS)
+			(*temp)->hd = here_doc((*temp));
 		else
-			token->hd = NULL;
-		if (token->op == NOT_OP)
-			env_var(token);
-		if (token->op == NOT_OP)
-			quote_handling(token->str);
-		token = token->next;
+			(*temp)->hd = NULL;
+		if ((*temp)->op == NOT_OP)
+			temp = env_var(temp);
+		if ((*temp)->op == NOT_OP)
+			quote_handling((*temp));
+		// free(glb_list()->quotes_index);
+		if ((*temp) == NULL)
+			break;
+		temp = &(*temp)->next;
 	}
 }
