4099c0958069c7c6119b4e892daaeddfb7fd5f67
SAADELJARIDA <saadeljarida@gmail.com>
2025-08-07 14:35:08 +0100

    sure

--- main.c
+++ main.c
@@ -58,17 +58,17 @@ int main(int ac, char **av, char **env)
 
     t_tokenizer *tokens;
     char *input;
-    // int exit_status;
-    // char **args;
-    // char **envp;
-	// char *path;
+    int exit_status;
+    char **args;
+    char **envp;
+	char *path;
 
   (void)ac;
   (void)av;
 
-//   glb_list()->env = NULL;
-//   init_env_var(&glb_list()->env, env);
-  glb_list()->env = save_env(env); 
+  glb_list()->env = NULL;
+  init_env_var(&glb_list()->env, env);
+//   glb_list()->env = save_env(env); 
   while (1) {
     input = readline("\033[1;32m➜\033[0m\033[1;36m Minishell $> \033[0m");
 	if (input_error(input) == 1)
@@ -86,50 +86,50 @@ int main(int ac, char **av, char **env)
     print_tokenizer(tokens);
 	redirection_infos(tokens);	
 	//error
-	// args = tokens_to_args(tokens);
-	// if (args && args[0])
-	// {
-	// 	exit_status = 0;
-	// 	if (execute_builtin(args, &glb_list()->env, &exit_status) == 1)
-	// 		printf("Builtin command executed successfully.\n");
-	// 	else
-	// 	{
-	// 		path = get_cmd_path(args[0], glb_list()->env);
-	// 		if (!path)
-	// 		{
-	// 			ft_putstr_fd("minishell: command not found: ", 2);
-	// 			ft_putstr_fd(args[0], 2);
-	// 			ft_putchar_fd('\n', 2);
-	// 			exit_status = 127;
-	// 		}
-	// 		else
-	// 		{
-	// 			envp = envlist_to_array(glb_list()->env);
-	// 			if (!envp)
-	// 			{
-	// 				perror("envlist_to_array");
-	// 				free(path);
-	// 				exit(1);
-	// 			}
-	// 			if (fork() == 0)
-	// 			{
-	// 				execve(path, args, envp);
-	// 				perror("execve");
-	// 				exit(1);
-	// 			}
-	// 			else
-	// 			{
-	// 				wait(&exit_status);
-	// 			}
-	// 			free(path);
-	// 			free_strs(envp);
-	// 		}
-	// 	}
+	args = tokens_to_args(tokens);
+	if (args && args[0])
+	{
+		exit_status = 0;
+		if (execute_builtin(args, &glb_list()->env, &exit_status) == 1)
+			printf("Builtin command executed successfully.\n");
+		else
+		{
+			path = get_cmd_path(args[0], glb_list()->env);
+			if (!path)
+			{
+				ft_putstr_fd("minishell: command not found: ", 2);
+				ft_putstr_fd(args[0], 2);
+				ft_putchar_fd('\n', 2);
+				exit_status = 127;
+			}
+			else
+			{
+				envp = envlist_to_array(glb_list()->env);
+				if (!envp)
+				{
+					perror("envlist_to_array");
+					free(path);
+					exit(1);
+				}
+				if (fork() == 0)
+				{
+					execve(path, args, envp);
+					perror("execve");
+					exit(1);
+				}
+				else
+				{
+					wait(&exit_status);
+				}
+				free(path);
+				free_strs(envp);
+			}
+		}
 
-	// }
+	}
 //	print_env(glb_list()->env);
-	//free_args(args);
+	free_args(args);
+	free_tokens(input, tokens);
 	}
 	// free_env ???
-	free_tokens(input, tokens);
 }
